08ef637332c1fe610a086b84274ed4f4ce6b82c0
Gamid <gamaizingg@gmail.com>
2023-05-16 12:02:18 +0300

    отредактировал стили

--- src/features/Menu/ui/Menu.tsx
+++ src/features/Menu/ui/Menu.tsx
@@ -22,9 +22,17 @@ const Menu = () => {
         <li className="nav__item">
           <Link
             className={`nav__link ${montserrat.className}`}
-            href={"/collection"}
+            href={"/"}
           >
             <span className="nav__text">Коллекция</span>
+          </Link>
+        </li>
+        <li className="nav__item">
+          <Link
+            className={`nav__link ${montserrat.className}`}
+            href={"/"}
+          >
+            <span className="nav__text">Виртуал тур</span>
           </Link>
         </li>
         <li className="nav__item">
