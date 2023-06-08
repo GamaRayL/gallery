10df8061581ac36ffb0c491c438b8a4d41ce897f
Gamid <gamaizingg@gmail.com>
2023-06-08 14:00:19 +0300

    Тест

--- src/pages_flat/Artwork/ui/Artwork.tsx
+++ src/pages_flat/Artwork/ui/Artwork.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 import { observer } from "mobx-react-lite";
 import { AnimatePresence, motion } from "framer-motion";
 import { BiCube } from "react-icons/bi";
-import toolsStore from "store/ToolsStore";
+import tools from "store/ToolsStore";
 import { Layout } from "widgets";
 import { infoChildVariants } from "pages_flat/Artwork/lib/utils";
 import Carousel from "pages_flat/Artwork/Carousel/ui/Carousel";
@@ -40,9 +40,9 @@ const Artwork: FC<IArtworkTistDataSingle> = observer(({ artwork, artist }) => {
           <div className="artwork__tool">
             <Button
               className="artwork__tool-btn"
-              onClick={() => toolsStore.toggleOrbitControls()}
+              onClick={() => tools.toggleOrbitControls()}
               icon={<BiCube
-                size={toolsStore.isOrbitControls ? 64 : 44}
+                size={tools.isOrbitControls ? 64 : 44}
                 color="white" />} />
           </div>
 
@@ -66,7 +66,7 @@ const Artwork: FC<IArtworkTistDataSingle> = observer(({ artwork, artist }) => {
           </div>
 
           <AnimatePresence>
-            {toolsStore.isOrbitControls &&
+            {tools.isOrbitControls &&
               <Popup className="artwork__popup" color="black" bgColor="white">
                 <motion.p
                   variants={infoChildVariants}
