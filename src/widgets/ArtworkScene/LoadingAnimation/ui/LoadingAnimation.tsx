4e9d4ba1014084c4e3aa7dff56158ae290481730
Gamid <gamaizingg@gmail.com>
2023-06-12 16:57:58 +0300

    Обновлена анимация загрузки перед отображением изображения, а также отступы к header

--- src/widgets/ArtworkScene/LoadingAnimation/ui/LoadingAnimation.tsx
+++ src/widgets/ArtworkScene/LoadingAnimation/ui/LoadingAnimation.tsx
@@ -1,21 +1,36 @@
 import { useRef } from "react";
 import { Mesh } from "three";
 import { useFrame } from "@react-three/fiber";
+import { Box, Cone, Sphere } from "@react-three/drei";
 
 const LoadingAnimation = () => {
   const meshRef = useRef<Mesh>(null);
 
   useFrame(() => {
     if (meshRef.current) {
-      meshRef.current.rotation.y += 0.01;
+      meshRef.current.rotation.y += 0.04;
+      meshRef.current.rotation.x += 0.04;
     }
   });
 
   return (
-    <mesh ref={meshRef} scale={[200, 200, 200]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color="gray" />
-    </mesh>
+    <>
+      <directionalLight
+        position={[100, 100, 0]}
+        castShadow
+      />
+      <group>
+        <Sphere receiveShadow scale={[25, 25, 25]} position={[-100, 0, 0]}>
+          <meshStandardMaterial color="white" />
+        </Sphere>
+        <Cone receiveShadow scale={[40, 40, 40]}>
+          <meshStandardMaterial color="orange" />
+        </Cone>
+        <Box receiveShadow ref={meshRef} scale={[40, 40, 40]} position={[100, 0, 0]}>
+          <meshStandardMaterial color="#363431" />
+        </Box>
+      </group>
+    </>
   );
 };
 
