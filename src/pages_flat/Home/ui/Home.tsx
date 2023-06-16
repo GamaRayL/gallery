8d9eaa08f27143c2d6f0cf36fa414fd235a52c41
Gamid <gamaizingg@gmail.com>
2023-06-16 15:52:53 +0300

    Overflow тест на ios

--- src/pages_flat/Home/ui/Home.tsx
+++ src/pages_flat/Home/ui/Home.tsx
@@ -45,10 +45,9 @@ const Home: FC<IArtworksData> = observer(({ artworks }) => {
 
   return (
     <Layout title="Главная" description="Главная страница частной галереи Омара Муртузалиевича.">
-      <Container>
+      <Container className="home-container">
         <section>
           <Grid className="home" columns={2}>
-
             <AnimatePresence>
               {!toolsStore.isSearch &&
                 <motion.div
